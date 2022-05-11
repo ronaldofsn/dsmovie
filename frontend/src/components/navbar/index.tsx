6b2841159261a10b5ddf677ef58a88b26301363c
ronaldofsn <ronaldofsn@gmail.com>
2022-05-10 21:59:58 -0300

    Navbar

---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,24 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css'
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/ronaldofsn/">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/ronaldofsn</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+
+export default Navbar;
