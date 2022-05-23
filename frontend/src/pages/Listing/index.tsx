6fb9e412e93ec02070dec7b8512c00c456346733
ronaldofsn <ronaldofsn@gmail.com>
2022-05-23 20:57:28 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,28 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    //FORMA ERRADA
-    axios.get('http://localhost:8080/movies?size=12&page=0')
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=5`)
         .then(response => {
-            console.log(response.data);
-        })
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
+        });
+    }, [])
+    //FORMA ERRADA
+ 
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
